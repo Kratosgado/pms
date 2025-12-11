3ffad10da809ab448fd613a45d0b8671e011c51a
Kratosgado <mbeahessilfieprince@gmail.com>
2025-12-11 08:29:04 +0000

    refactor(module-3): seed to use hashmap instead of arraylist

--- src/main/java/com/kratosgado/pms/utils/Seed.java
+++ src/main/java/com/kratosgado/pms/utils/Seed.java
@@ -2,6 +2,8 @@
 package com.kratosgado.pms.utils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
 import com.kratosgado.pms.models.AdminUser;
 import com.kratosgado.pms.models.HardwareProject;
@@ -10,62 +12,75 @@ import com.kratosgado.pms.models.RegularUser;
 import com.kratosgado.pms.models.SoftwareProject;
 import com.kratosgado.pms.models.Task;
 import com.kratosgado.pms.models.User;
+import com.kratosgado.pms.utils.enums.TaskStatus;
 
 public class Seed {
+  private static final Random random = new Random();
 
-  public static User[] seedUsers() {
-    final ArrayList<User> users = new ArrayList<>();
+  public static HashMap<String, User> seedUsers() {
+    final HashMap<String, User> users = new HashMap<>();
     final String prefix = "US";
     String id = CustomUtils.getNextId(prefix, users.size());
     User newUser = new AdminUser(id, "Kratos", "kratos@gmail.com", "password");
-    users.add(newUser);
+    users.put(newUser.getId(), newUser);
     id = CustomUtils.getNextId(prefix, users.size());
     newUser = new RegularUser(id, "Mbeah", "mbeah@gmail.com", "password");
-    users.add(newUser);
+    users.put(newUser.getId(), newUser);
     id = CustomUtils.getNextId(prefix, users.size());
     newUser = new RegularUser(id, "Prince", "prince@gmail.com", "password");
-    users.add(newUser);
+    users.put(newUser.getId(), newUser);
     id = CustomUtils.getNextId(prefix, users.size());
     newUser = new RegularUser(id, "Gado", "gado@gmail.com", "password");
-    users.add(newUser);
+    users.put(newUser.getId(), newUser);
 
-    return users.toArray(new User[0]);
+    return users;
   }
 
-  public static Project[] seedProjects() {
-    final ArrayList<Project> projects = new ArrayList<>();
+  public static HashMap<String, Project> seedProjects() {
+    final HashMap<String, Project> projects = new HashMap<>();
     final String prefix = "PJ";
     String id = CustomUtils.getNextId(prefix, projects.size());
     Project project = new SoftwareProject(id, "PMS", "Project management system", 4, 1000.00);
-    projects.add(project);
+    project.setTasks(seedTasks(project.getName()));
+    projects.put(project.getId(), project);
+
+    id = CustomUtils.getNextId(prefix, projects.size());
+    project = new SoftwareProject(id, "Steganography", "Hiding information in images", 4, 1000.00);
+    project.setTasks(seedTasks(project.getName()));
+    projects.put(project.getId(), project);
 
     id = CustomUtils.getNextId(prefix, projects.size());
     project = new HardwareProject(id, "Psuedo Interpreter", "Project management system", 7, 2000.00);
-    projects.add(project);
+    project.setTasks(seedTasks(project.getName()));
+    projects.put(project.getId(), project);
     id = CustomUtils.getNextId(prefix, projects.size());
     project = new HardwareProject(id, "Psuedo Runner", "Project management system", 11, 13000.00);
-    projects.add(project);
+    project.setTasks(seedTasks(project.getName()));
+    projects.put(project.getId(), project);
     id = CustomUtils.getNextId(prefix, projects.size());
     project = new SoftwareProject(id, "Console", "Project management system", 9, 2000.00);
+    project.setTasks(seedTasks(project.getName()));
+    projects.put(project.getId(), project);
 
-    projects.add(project);
-    id = CustomUtils.getNextId(prefix, projects.size());
-    project = new SoftwareProject(id, "TSM", "Task management system", 4, 5000.00);
-    projects.add(project);
-    return projects.toArray(new Project[0]);
+    return projects;
   }
 
-  public static Task[] seedTasks() {
+  private static TaskStatus getRandomTaskStatus() {
+    return TaskStatus.values()[random.nextInt() % TaskStatus.values().length];
+  }
+
+  private static ArrayList<Task> seedTasks(String project) {
     final ArrayList<Task> tasks = new ArrayList<>();
-    final String prefix = "TS";
-    for (int i = 0; i < 5; i++) {
-      String projectId = CustomUtils.getNextId("PJ", i);
-      for (int j = 0; j < 3; j++) {
-        String id = CustomUtils.getNextId(prefix, tasks.size());
-        Task task = new Task(id, "Task " + (i * 3 + j), projectId);
-        tasks.add(task);
-      }
-    }
-    return tasks.toArray(new Task[0]);
+    String id = CustomUtils.getNextId("TS", tasks.size());
+    tasks.add(new Task(id, project + " task 1", getRandomTaskStatus()));
+    id = CustomUtils.getNextId("TS", tasks.size());
+    tasks.add(new Task(id, project + " task 2", getRandomTaskStatus()));
+    id = CustomUtils.getNextId("TS", tasks.size());
+    tasks.add(new Task(id, project + " task 3", getRandomTaskStatus()));
+    id = CustomUtils.getNextId("TS", tasks.size());
+    tasks.add(new Task(id, project + " task 4", getRandomTaskStatus()));
+    id = CustomUtils.getNextId("TS", tasks.size());
+    tasks.add(new Task(id, project + " task 5", getRandomTaskStatus()));
+    return tasks;
   }
 }
